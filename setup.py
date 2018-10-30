cf3f9fb83f7575b1bb6926b55f07dcb6b412baf0
Jeroen van Heugten <jeroen@byte.nl>
2018-10-30 15:55:49 +0100

    Bump version to v0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = [
 
 setup(
     name='searchguard',
-    version='0.1.1',
+    version='0.2',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/ByteInternet/searchguard-python',
     author='Jeroen van Heugten',
