f21e66c5d03b60950a543e4ca711ff13a9da884c
Jeroen van Heugten <jeroen@byte.nl>
2018-11-14 15:33:37 +0100

    Bump version to v0.4

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 
 setup(
     name='searchguard',
-    version='0.3',
+    version='0.4',
     packages=find_packages(exclude=['tests']),
     license='MIT',
     url='https://github.com/ByteInternet/searchguard-python',
