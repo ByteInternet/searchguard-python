25ecb655b5b162ff9e331b16ae8067e76d2f99be
Farzad Ghanei <farzad.ghanei@gmail.com>
2018-11-23 14:48:17 +0100

    settings use empty string if environment variables are missing
    we use the environment variables by default, but if they are not set
    we can just use empty strings. Of course in production empty strings
    are not going to work, but we give the opportunity for the settings
    to be set to the settings module in runtime, specially helpful
    for tests.

--- searchguard/settings.py
+++ searchguard/settings.py
@@ -1,5 +1,5 @@
 import os
 
 HEADER = {'content-type': 'application/json'}
-SGAPI = os.environ['SEARCHGUARD_API_URL']
-TOKEN = (os.environ['SEARCHGUARD_API_USER'], os.environ['SEARCHGUARD_API_PASS'])
+SGAPI = os.environ.get('SEARCHGUARD_API_URL', '')
+TOKEN = (os.environ.get('SEARCHGUARD_API_USER', ''), os.environ.get('SEARCHGUARD_API_PASS', ''))
