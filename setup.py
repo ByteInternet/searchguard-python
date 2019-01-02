934ab19b1fad00b62fad4e535e51899668ad4b06
Jeroen van Heugten <jeroen@byte.nl>
2019-01-02 16:10:40 +0100

    Bump version to v0.8.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 
 setup(
     name='searchguard',
-    version='0.8',
+    version='0.8.1',
     packages=find_packages(exclude=['tests']),
     license='MIT',
     url='https://github.com/ByteInternet/searchguard-python',
