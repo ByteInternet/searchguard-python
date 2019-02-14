e2fbd01c0e561bf5009a42dc1b04b93c3ca0d727
Ben Konrath <ben.konrath@byte.nl>
2019-02-14 11:36:19 +0100

    Change requests requirement to be >= 2.20
    Using >= is better than using a hard requirement because this allows applications to select which version they want to use.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        'requests==2.20'
+        'requests>=2.20'
     ],
 )
 
